b065685b4fef36e7c775574308cf9f75b6bfaf1f
unknown <cjlg985@gmail.com>
2021-12-10 15:58:10 -0400

    [ADD] GitHub Actions

---
+++ src/containers/MyOrder.jsx
@@ -0,0 +1,44 @@
+import React,{useContext} from 'react';
+import OrderItem from '@components/OrderItem';
+import '@styles/MyOrder.scss';
+
+import arrowImg from "@icons/flechita.svg";
+import AppContext from './../context/AppContext';
+
+
+const MyOrder = () => {
+	const {state} = useContext(AppContext);
+
+	const sumTotal = ()=>{
+		const reducer = (accumulator,currentValue)=>accumulator+currentValue.price;
+		const sum = state.cart.reduce(reducer,0);
+		return sum;
+
+	}
+
+	return (
+		<aside className="MyOrder">
+			<div className="title-container">
+				<img src={arrowImg} alt="arrow" />
+				<p className="title">My order</p>
+			</div>
+			<div className="my-order-content">
+				{state.cart.map(product=>(
+					<OrderItem  product ={product} key={`OrderItem-${product.id}`}/>
+				))}
+				
+				<div className="order">
+					<p>
+						<span>Total</span>
+					</p>
+					<p>${sumTotal()}</p>
+				</div>
+				<button className="primary-button">
+					Checkout
+				</button>
+			</div>
+		</aside>
+	);
+}
+
+export default MyOrder;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Header from "@components/Header";
+import Menu from "@components/Menu";
+import ProductItem from "@components/ProductItem";
+
+import  ProductList  from '@containers/ProductList';
+ const Home = ()=>{
+     return (
+         <>
+            <Header/>
+             <ProductList/>
+         
+         </>
+                
+           
+      
+        
+      
+     );
+ }
+
+ export default  Home;
